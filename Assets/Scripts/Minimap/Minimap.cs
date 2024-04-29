bb623990fda316304323baccc7e25669c208c7ce
Santax19 <146781067+Santax19@users.noreply.github.com>
2024-04-29 19:44:25 -0300

    se añadio script de minimapa

---
+++ Assets/Scripts/Minimap/Minimap.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public Transform player;
+
+
+    private void LateUpdate()
+    {
+        Vector3 position = player.position;
+
+        position.y = transform.position.y;
+
+        transform.position = position;
+
+    }
+}
