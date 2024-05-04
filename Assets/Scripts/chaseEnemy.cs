168a5184375c84ed4878444d16836e4866e68d99
Santax19 <146781067+Santax19@users.noreply.github.com>
2024-05-04 18:21:50 -0300

    added minigolem script

--- Assets/Scripts/chaseEnemy.cs
+++ Assets/Scripts/chaseEnemy.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class chaseEnemy : MonoBehaviour
@@ -7,9 +5,12 @@ public class chaseEnemy : MonoBehaviour
     public Transform player;
     public float rangoVision;
     public float speed;
-
     public Vector3[] positions;
     public int index;
+    public Transform rotationPoint;
+    public float rotationSpeed;
+    public float rotationRange;
+
     void Start()
     {
         
@@ -18,22 +19,39 @@ public class chaseEnemy : MonoBehaviour
     
     void Update()
     {
-        if(Vector3.Distance(transform.position, player.position) < rangoVision)
+        
+
+        if (Vector3.Distance(transform.position, player.position) < rangoVision)
         {
+            Debug.Log("Te encontre wachin");
             Vector3 playerDirection = (player.position - transform.position).normalized;
             playerDirection.y = 0;
 
-            transform.position += playerDirection*speed*Time.deltaTime;
+            transform.position += playerDirection * speed * Time.deltaTime;
         }
         else
         {
+            Debug.Log("Me hago la paja" + transform.position);
             Vector3 positionDirection = (positions[index] - transform.position).normalized;
             positionDirection.y = 0;
 
+            transform.position = Vector3.MoveTowards(transform.position, positions[index],speed * Time.deltaTime);
+
             transform.position += positionDirection * speed * Time.deltaTime;
 
-           
+            if (Vector3.Distance(transform.position,positions[index]) < 0.1f)
+            {
+                index++;
+                if(index >= positions.Length)
+                {
+                    index = 0;
+                }
+            }
         }
+        
+
+
+
     }
     public void OnDrawGizmosSelected()
     {
